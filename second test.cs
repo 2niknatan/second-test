073a9443e5ed079f355ebea57e8afb72497ca47c
2niknatan <109152039+2niknatan@users.noreply.github.com>
2023-03-26 14:19:23 +0300

    test

---
+++ second test.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!\nPress any key to coninue...");
+            Console.ReadLine();
+        }
+    }
+}
